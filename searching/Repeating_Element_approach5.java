140b142565ed0b03a63cdadf548e451ff1d873c4
Mr verma <67165121+mrverma441@users.noreply.github.com>
2022-07-13 09:08:14 +0530

    commit

--- searching/Repeating_Element_approach5.java
+++ searching/Repeating_Element_approach5.java
@@ -3,26 +3,28 @@ package Searching;
 public class Repeating_Element_approach5 {
 	public static int findRepating(int[]array)
 	{
-	
-		int slow=array[0],fast=array[0];
+		int n=array.length;
+		int slow=array[0]+1,fast=array[0]+1;
 		do
 		{
-			slow=array[slow];
-			fast=array[array[fast]];
+			slow=array[slow]+1;
+			fast=array[array[fast]+1]+1;
 			
 		}while(slow!=fast);
-		slow=array[0];
+		
+		
+		slow=array[0]+1;
 		while(slow!=fast)
 		{
-			slow=array[slow];
-			fast=array[fast];
+			slow=array[slow]+1;
+			fast=array[fast]+1;
 		}
-		return slow;
-	
-	
+		return slow-1;
 	}
 	public static void main(String[] args) {
-		int[]array= {1,3,2,4,3,3};
+		int[]array= {0,2,1,3,2,2};
+		int[]array2= {1,2,3,0,3,4,5};
+		int[]array3= {0,0};
 		System.out.println(findRepating(array));
 		}
 	
